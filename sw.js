f0acfde0db55c0853ea828296b3119e0339a70ac
Masterhoopla <dtvdan2014@gmail.com>
2025-08-28 10:04:11 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,12 +1,21 @@
-const CACHE = 'cctv-cache-v1';
+// SW v2: bump cache name + immediate activation
+const CACHE = 'cctv-cache-v2';
 const ASSETS = ['./','./index.html','./style.css','./app.js','./manifest.json'];
 
 self.addEventListener('install', (e) => {
+  self.skipWaiting();
   e.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
 });
 
 self.addEventListener('activate', (e) => {
-  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
+  e.waitUntil(Promise.all([
+    caches.keys().then(keys => Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))),
+    self.clients.claim()
+  ]));
+});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') self.skipWaiting();
 });
 
 self.addEventListener('fetch', (e) => {
